7f046977725fa0ec1ae1f4e14931cf308afd65e5
saim85 <saimshahsadiq@gmail.com>
2023-06-04 06:20:02 -0700

    Intial Commit

---
+++ HTML_Files/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Anton&family=Caveat&family=DM+Sans:ital,wght@0,500;1,500&family=Indie+Flower&family=Pacifico&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../CSS_Files/header.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            <a href="#"><img src="../images/logo.png" alt="Logo"></a>
+        </div>
+        <ul class="navbar-list">
+            <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Process</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Finishing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">ToolBox</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">FAQ</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+            <li class="nav-item"><a href="#" class="nav-link login">LogIn</a></li>
+        </ul>
+        <div class="menu-icon">
+        <i class="fas fa-bars"></i>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ HTML_Files/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../CSS_Files/index.css">
+</head>
+<body>
+    <?php
+    include("header.php");
+    ?>
+    <header class="header">
+        <div class="top-container">
+            <p>STANDARD DELIVERY: <span>2-3 BUSINESS DAYS</span></p>
+            <p>NEED ADVICE? <span>01527 958004</span> Mon - Fri 07:30 to 16:00 (GMT)</p>
+        </div>
+
+        <section class="hero">
+            <h2 class="first-heading">Print & Bind Online</h2>
+         <div class="hero-container">
+            <div class="first-con">
+            <div class="first-con-content">
+                <div class="left-first">
+                    <h3><span>Brochures</span> saddle stitched</h3>
+                    <h4><span>What</span> distinguishes</h4>
+                    <h5>Our Brochures</h5>
+                    <a href="#">More Information</a>
+                </div>
+                <div class="right-first">
+                    <img src="../images/hero1.png" alt="hero-img">
+                </div>
+            </div>
+            </div>
+
+            <div class="hero-card">
+                <div class="card">
+                    <header>Flyers & Leaflets</header>
+                    <div class="center">
+                        <img src="../images/flyer.png" alt="">
+                    </div>
+                    <footer>
+                        <div class="left-content">
+                        <i class="fas fa-calculator"></i>
+                            <span>Calculate Price</span>
+                        </div>
+                        <div class="right-content"><i class="fas fa-chevron-right"></i></div>
+                    </footer>
+                </div>
+                <div class="card">
+                    <header>Flyers & Leaflets</header>
+                    <div class="center">
+                        <img src="../images/flyer.png" alt="">
+                    </div>
+                    <footer>
+                        <div class="left-content">
+                        <i class="fas fa-calculator"></i>
+                            <span>Calculate Price</span>
+                        </div>
+                        <div class="right-content"><i class="fas fa-chevron-right"></i></div>
+                    </footer>
+                </div>
+                <div class="card">
+                    <header>Flyers & Leaflets</header>
+                    <div class="center">
+                        <img src="../images/flyer.png" alt="">
+                    </div>
+                    <footer>
+                        <div class="left-content">
+                        <i class="fas fa-calculator"></i>
+                            <span>Calculate Price</span>
+                        </div>
+                        <div class="right-content"><i class="fas fa-chevron-right"></i></div>
+                    </footer>
+                </div>
+                <div class="card">
+                    <header>Flyers & Leaflets</header>
+                    <div class="center">
+                        <img src="../images/flyer.png" alt="">
+                    </div>
+                    <footer>
+                        <div class="left-content">
+                        <i class="fas fa-calculator"></i>
+                            <span>Calculate Price</span>
+                        </div>
+                        <div class="right-content"><i class="fas fa-chevron-right"></i></div>
+                    </footer>
+                </div>
+                <div class="card">
+                    <header>Flyers & Leaflets</header>
+                    <div class="center">
+                        <img src="../images/flyer.png" alt="">
+                    </div>
+                    <footer>
+                        <div class="left-content">
+                        <i class="fas fa-calculator"></i>
+                            <span>Calculate Price</span>
+                        </div>
+                        <div class="right-content"><i class="fas fa-chevron-right"></i></div>
+                    </footer>
+                </div>
+            </div>
+
+         </div>
+        </section>
+    </header>
+
+    <main class="main">
+        <div class="main-container">
+            <div class="first-cards-con">
+                <div class="card">
+                    <div class="heading">
+                        <h2>Letterheads</h2>
+                    </div>
+                    <div class="new">
+                        <div class="order">order</div>
+                        <div class="product">Product</div>
+                    </div>
+                    <div class="icon">
+                    <i class="fas fa-chevron-right"></i>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="heading">
+                        <h2>Letterheads</h2>
+                    </div>
+                    <div class="new">
+                        <div class="order">order</div>
+                        <div class="product">Product</div>
+                    </div>
+                    <div class="icon">
+                    <i class="fas fa-chevron-right"></i>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="heading">
+                        <h2>Letterheads</h2>
+                    </div>
+                    <div class="new">
+                        <div class="order">order</div>
+                        <div class="product">Product</div>
+                    </div>
+                    <div class="icon">
+                    <i class="fas fa-chevron-right"></i>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="heading">
+                        <h2>Letterheads</h2>
+                    </div>
+                    <div class="new">
+                        <div class="order">order</div>
+                        <div class="product">Product</div>
+                    </div>
+                    <div class="icon">
+                    <i class="fas fa-chevron-right"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
